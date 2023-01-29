ec496cd679020f7c0bc5fd84c23e4c6a4daf4b73
Soberek <59960384+Soberek@users.noreply.github.com>
2023-01-29 03:47:43 +0100

    Passed props to FoodItem, Created Reducer for handling Food List

--- src/Layout/FoodList.tsx
+++ src/Layout/FoodList.tsx
@@ -1,14 +1,53 @@
+import { useReducer } from "react"
 import styles from "./FoodList.module.scss"
 import FoodItem from "../components/FoodItem"
+
+import mealsData from "../dummy_data"
+
+export interface FoodItemInterface {
+  id: string
+  name: string
+  description: string
+  price: number
+  amount?: number
+}
+
 function FoodList() {
+  const [meals, dispatchMeals] = useReducer(
+    (
+      state: FoodItemInterface[],
+      action: { type: string; meal: FoodItemInterface }
+    ): FoodItemInterface[] => {
+      if (action.type === "add-meal") {
+        if (state.find((el) => el.id === action.meal.id)) {
+          console.log("Meal exist.")
+          return [
+            ...state.map((el) => {
+              return { ...el, amount: el.amount ? el.amount++ : 0 }
+            }),
+          ]
+        }
+        return [...state, { ...action.meal, amount: 1 }]
+      }
+
+      return state
+    },
+    []
+  )
+
   return (
     <div className={styles.foodList}>
       <div className={styles.foodList__container}>
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
-        <FoodItem />
+        {mealsData.map((meal) => (
+          <FoodItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+            addMealHandler={dispatchMeals}
+          />
+        ))}
       </div>
     </div>
   )
--- src/components/FoodItem.tsx
+++ src/components/FoodItem.tsx
@@ -1,18 +1,33 @@
+import { Dispatch } from "react"
 import styles from "./FoodItem.module.scss"
 
-function FoodItem() {
+import { FoodItemInterface } from "../Layout/FoodList"
+
+interface FoodItemProps extends FoodItemInterface {
+  addMealHandler: Dispatch<{ type: string; meal: FoodItemInterface }>
+}
+
+function FoodItem({ id, name, description, price, addMealHandler }: FoodItemProps) {
+  // console.log(addMealHandler())
+
   return (
     <div className={styles.foodItem}>
       <div className={styles.foodItem__description}>
-        <div className={styles.foodItem__description__title}>Sushi</div>
-        <div className={styles.foodItem__description__dummy_text}>Fines fish and veggies</div>
-        <div className={styles.foodItem__description__price}>$22.99</div>
+        <div className={styles.foodItem__description__title}>{name}</div>
+        <div className={styles.foodItem__description__dummy_text}>{description}</div>
+        <div className={styles.foodItem__description__price}>${price}</div>
       </div>
       <div className={styles.foodItem__order_functionality}>
         <h5 className={styles.foodItem__order_functionality__amount}>Amount</h5>
-        <input className={styles.foodItem__order_functionality__count} value={5} />
+        <input type='number' className={styles.foodItem__order_functionality__count} />
         <div className={styles.foodItem__order_functionality__button}>
-          <button>+ Add</button>
+          <button
+            onClick={() =>
+              addMealHandler({ type: "add-meal", meal: { id, name, description, price } })
+            }
+          >
+            + Add
+          </button>
         </div>
       </div>
     </div>
