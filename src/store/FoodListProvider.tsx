1c9c198651b343224fafe975ccc76b9e2cb2309e
Soberek <59960384+Soberek@users.noreply.github.com>
2023-01-30 05:18:25 +0100

    Moved Reducer functionality from FoodList component to FoodListProvider Store

---
+++ src/store/FoodListProvider.tsx
@@ -0,0 +1,79 @@
+import { createContext, useReducer } from "react"
+
+// ======================================================================================
+// FoodList Context Provider
+// ======================================================================================
+
+import mealsData from "../dummy_data"
+
+export interface IFoodItemInterface {
+  id: string
+  name: string
+  description: string
+  price: number
+  amount: number
+}
+
+interface IFoodList {
+  meals: IFoodItemInterface[]
+  handleMealsChange: ({}: handleMealsChangeType) => void
+}
+
+interface IFoodListProviderProps {
+  children: React.ReactNode
+}
+
+export type handleMealsChangeType = { type: string; meal: { id: string; addValue: number } }
+
+const defaultFoodList: IFoodList = {
+  meals: [],
+  handleMealsChange: () => {},
+}
+
+// Context
+export const FoodListContext = createContext(defaultFoodList)
+
+export const FoodListProvider = ({ children }: IFoodListProviderProps) => {
+  // Reducer
+  const [meals, dispatchMeals] = useReducer(foodListReducer, [
+    ...mealsData.map((meal) => {
+      return { ...meal, amount: 0 }
+    }),
+  ])
+
+  function handleMealsChange({ type, meal }: handleMealsChangeType) {
+    dispatchMeals({ type, meal })
+  }
+
+  return (
+    <FoodListContext.Provider value={{ meals, handleMealsChange }}>
+      {children}
+    </FoodListContext.Provider>
+  )
+}
+
+function foodListReducer(
+  state: IFoodItemInterface[],
+  action: handleMealsChangeType
+): IFoodItemInterface[] {
+  // Adding meal
+
+  switch (action.type) {
+    case "add-meal":
+      return [
+        ...state.map((el) => {
+          if (el.id === action.meal.id) {
+            console.log(action.meal.addValue, el.id)
+            return {
+              ...el,
+              amount: el.amount ? el.amount + action.meal.addValue : action.meal.addValue,
+            }
+          } else {
+            return el
+          }
+        }),
+      ]
+    default:
+      throw new Error("You shouldn't be here...")
+  }
+}
